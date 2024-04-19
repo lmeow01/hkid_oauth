8e79fd80d04eed448a1b038d568158e20d3fc8fa
lmeow01 <65549164+lmeow01@users.noreply.github.com>
2024-04-20 05:10:33 +0800

    Magic Link api finally set up completely

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const R = require("ramda");
 const sgMail = require('@sendgrid/mail')
+const nodemailer = require('nodemailer');
 const { verifyAuthToken } = require("../middlewares/authenticate");
 // const projectMiddleware = require("../middlewares/projectMiddleware");
 const User = require("../models/user");
-
 const router = new express.Router();
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "hkidoauth@gmail.com",
+    pass: process.env.GOOGLE_APP_PASSWORD
+  },
+});
 
 
 router.post("/register", async (req, res) => {
@@ -25,22 +30,21 @@ router.post("/register", async (req, res) => {
 
 router.route("/login").post(async function (req, res) {
   var body = R.pick(["email", "projectID", "redirectURL", "scope"], req.body);
+
+
   try {
-    console.log(body)
     var user = await User.findByCredentials(body.email);
     var token = await user.generateAuthToken();
-    sgMail.send({
+
+    const info = await transporter.sendMail({
       to: body.email,
-      from: 'maple_pro@live.com',
+      from: 'hkidoauth@gmail.com',
       subject: 'Magic Link: User Authentication of HKID OAuth',
-      text: `Login to your HKID OAuth account with this link: https://hkid-frontend.vercel.app/authorization?token=${token}&projectID=${body.projectID}&redirectUrl=${body.redirectURL}&scope=${body.scope}`,
-    }).then((response) => {
-      if (response[0].statusCode === 202) {
-        res.status(200).send({"message": "Successfully generated and sent email!"});
-      } else {
-        res.status(400).send({ code: 400, message: response[0].body.errors[0].message  });
-      }
-    })  
+      text: `Login to your HKID OAuth account with this link: \n https://hkid-frontend.vercel.app/authorization?token=${token}&projectID=${body.projectID}&redirectUrl=${body.redirectURL}&scope=${body.scope}`
+    });
+
+    return res.status(200).send({"message": "Successfully generated and sent email!"});
+
     
   } catch (e) {
     console.log(e);
